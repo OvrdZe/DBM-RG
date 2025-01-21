50f3716cb3f50283752103f8c673352fe5814086
OvrdZe <ovrdze@gmail.com>
2025-01-21 09:16:22 +0100

    Update Razuvious.lua
    Adjusted timers based on old DBM RG.

--- DBM-Naxx/MilitaryQuarter/Razuvious.lua
+++ DBM-Naxx/MilitaryQuarter/Razuvious.lua
@@ -16,7 +16,7 @@ local warnShoutNow		= mod:NewSpellAnnounce(29107, 1)
 local warnShoutSoon		= mod:NewSoonAnnounce(29107, 3)
 local warnShieldWall	= mod:NewAnnounce("WarningShieldWallSoon", 3, 29061, nil, nil, nil, 29061)
 
-local timerShout		= mod:NewNextTimer(15, 29107, nil, nil, nil, 2)
+local timerShout		= mod:NewNextTimer(16, 29107, nil, nil, nil, 2) --|15|
 local timerTaunt		= mod:NewCDTimer(20, 29060, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 local timerShieldWall	= mod:NewCDTimer(20, 29061, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 local timerMindControl	= mod:NewBuffActiveTimer(60, 605, nil, nil, nil, 6)
